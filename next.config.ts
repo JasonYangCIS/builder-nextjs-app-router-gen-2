413a8e1d3a03708826ed21e04975145c2093651b
Jason Yang <jasonYangCIS@gmail.com>
2026-03-04 17:53:13 -0800

    feat: use webpack, init with builder devtools

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import BuilderDevTools from "@builder.io/dev-tools/next";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = BuilderDevTools()({
-  /* config options here */
-  turbopack: {},
-});
+const nextConfig: NextConfig = BuilderDevTools()(
+  BuilderDevTools()({
+    /* config options here */
+  }),
+);
 
 export default nextConfig;
