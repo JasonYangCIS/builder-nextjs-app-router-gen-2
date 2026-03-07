3454632246ed0a8e8ff0975e7f70d73802625daa
Jason Yang <jasonYangCIS@gmail.com>
2026-03-06 16:49:54 -0800

    feat: update catchall route to render builder content

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -1,21 +1,25 @@
-import { fetchOneEntry } from "@builder.io/sdk-react";
+import { fetchOneEntry, getBuilderSearchParams } from "@builder.io/sdk-react";
 import { RenderBuilderContent } from "@/components/builder";
 import { config } from "@/config";
+
+const builderModelName = config.models.page;
 
 export default async function Page(props: {
   params: Promise<{
     page: string[];
   }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const builderModelName = "page";
 
   // Use the page path specified in the URL to fetch the content
   const urlPath = "/" + ((await props?.params)?.page?.join("/") || "");
+  const searchParams = await props.searchParams;
 
   const content = await fetchOneEntry({
     // Get the page content from Builder with the specified options
     apiKey: config.envs.builderApiKey,
     model: builderModelName,
+    options: getBuilderSearchParams(searchParams as unknown as URLSearchParams),
     userAttributes: { urlPath },
   });
 
